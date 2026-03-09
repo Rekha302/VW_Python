8d59d79bdce671932316f0734c57406bca494560
Rekha Shekhawat <rekhashekhawat302@gmail.com>
2026-03-10 00:09:06 +1300

    question3

---
+++ ASSIGNMENT/day20/question3/app.py
@@ -0,0 +1,19 @@
+
+
+from flask import Flask
+from config import Config
+from models.book_model import db
+from routes.book_routes import book_bp
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(book_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ ASSIGNMENT/day20/sql.sql
@@ -0,0 +1,16 @@
+create DATABASE event_db;
+use event_db;
+select * from event;
+select * from registration;
+
+CREATE DATABASE order_db;
+USE order_db;
+select * from `order`;
+
+create DATABASE library_db;
+use library_db;
+
+select * from book;
+
+
+
